1bd2078becc5a460f40c138677e1f5c239710597
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2025-03-01 18:23:39 +0530

    Time: 60 ms (3.62%) | Memory: 45 MB (5.05%) - LeetSync

---
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        int c=0;
+        int ans=0;
+        for (int i = 2; i <= n - 2; i++) {
+            int temp = n;
+            StringBuilder sb = new StringBuilder();
+            while (temp > 0) {
+                sb.append(temp % i);
+                temp = temp / i;
+            }
+            if (sb.toString().equals(sb.reverse().toString())) {
+                c++;
+            }
+        }
+
+        return c == (n - 2);
+    }
+}
