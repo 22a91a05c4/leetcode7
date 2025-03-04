6a72584b8d30bf9055819beb04b7afc1815c0b8a
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2025-03-04 10:56:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (55.38%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         ArrayList<Boolean> al = new ArrayList<Boolean>();
-        int[] arr = new int[candies.length];
+        /*int[] arr = new int[candies.length];
         for(int i=0;i<candies.length;i++)
         {
             arr[i]=candies[i];
@@ -11,6 +11,25 @@ class Solution {
         for(int i=0;i<arr.length;i++)
         {
             if((arr[i]+extraCandies)>=maxele)
+            {
+                al.add(true);
+            }
+            else
+            {
+                al.add(false);
+            }
+        }*/
+        int maxele=candies[0];
+        for(int i=0;i<candies.length;i++)
+        {
+            if(candies[i]>=maxele)
+            {
+                maxele=candies[i];
+            }
+        }
+        for(int i=0;i<candies.length;i++)
+        {
+            if((candies[i]+extraCandies)>=maxele)
             {
                 al.add(true);
             }
