2bdde1649eeced09cf7ed197e7f3ad0bb6d91c7c
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2025-03-06 14:56:15 +0530

    Time: 2 ms (43.90%) | Memory: 41 MB (97.98%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        Arrays.sort(piles);
+        int n = piles.length/2;
+        int a=0,b=0;
+        for(int i=0;i<n;i++)
+        {
+            a=a+piles[i];
+        }
+        for(int i=n;i<piles.length;i++)
+        {
+            b=b+piles[i];
+        }
+        return a<b;
+    }
+}  
