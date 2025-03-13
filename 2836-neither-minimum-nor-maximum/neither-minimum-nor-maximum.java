748b9a23e8f1031ce585ccc4b0a592b010c6efa3
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2025-03-13 22:10:01 +0530

    Time: 4 ms (99.06%) | Memory: 45 MB (56.73%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
@@ -4,10 +4,16 @@ class Solution {
         {
             return -1;
         }
-        else
+        /*else
         {
             Arrays.sort(nums);
             return nums[1];
-        }
+        }*/
+        int a=nums[0];
+        int b=nums[1];
+        int c=nums[2];
+        if((a>b && a<c) || (a<b && a>c)) return a;
+        if((b>a && b<c) || (b<a && b>c)) return b;
+        return c;
     }
 }
