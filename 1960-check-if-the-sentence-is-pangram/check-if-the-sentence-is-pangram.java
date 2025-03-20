618e971a3d640d87299563a24383917dccf3501c
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2025-03-20 11:25:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (78.93%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,10 +1,16 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        HashSet<Character> hs = new HashSet<>();
+        /*HashSet<Character> hs = new HashSet<>();
         for(char ch : sentence.toCharArray())
         {
             hs.add(ch);
         }
-        return hs.size()==26;
+        return hs.size()==26;*/
+        if(sentence.length()<26)    return false;
+        for(char ch='a';ch<='z';ch++)
+        {
+            if(sentence.indexOf(ch)<0)  return false;
+        }
+        return true;
     }
 }
