5ffa790ff7a0a3fe56e00be7009e5b57c6b526e5
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2025-03-25 20:14:22 +0530

    Time: 3 ms (100.00%) | Memory: 77.3 MB (42.40%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        ArrayList<Integer> ps = new ArrayList<Integer>();
-        ArrayList<Integer> ne = new ArrayList<Integer>();
         int[] ans = new int[nums.length];
         int hl=nums.length/2;
+        /*ArrayList<Integer> ps = new ArrayList<Integer>();
+        ArrayList<Integer> ne = new ArrayList<Integer>();
         for(int i : nums)
         {
             if(i<0)
@@ -20,6 +20,21 @@ class Solution {
         {
             ans[k++]=ps.get(i);
             ans[k++]=ne.get(i);
+        }*/
+        int pi=0;
+        int ni=1;
+        for(int i:nums)
+        {
+            if(i>0) 
+            {
+                ans[pi]=i;
+                pi+=2;
+            }
+            else
+            {
+                ans[ni]=i;
+                ni+=2;
+            }
         }
         return ans;
     }
