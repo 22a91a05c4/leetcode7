dab72147ef0e75028ca0eb012531423e389583f8
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2025-08-21 23:03:32 +0530

    Time: 19 ms (36.44%) | Memory: 59.5 MB (24.42%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,31 @@
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+//         int n=nums.length;
+//         for (int i = 0; i < n - 1; i++) {
+//             if(nums[i]==nums[i+1])
+//             {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for(int i : nums)
+        {
+            hs.add(i);
+        }
+        if(nums.length!=hs.size())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
