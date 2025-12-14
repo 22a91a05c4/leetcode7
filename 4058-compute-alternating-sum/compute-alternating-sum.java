38aa3adfa60dc89db5e0cd859073ec487155169e
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2025-12-14 21:13:55 +0530

    Time: 1 ms (95.70%) | Memory: 46.6 MB (9.38%) - LeetSync

--- 4058-compute-alternating-sum/compute-alternating-sum.java
+++ 4058-compute-alternating-sum/compute-alternating-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int alternatingSum(int[] nums) {
         int ans=0;
-        for(int i=0;i<nums.length;i++)
+        /*for(int i=0;i<nums.length;i++)
         {
             if(i==0 || i%2==0)
             {
@@ -11,7 +11,17 @@ class Solution {
             {
                 ans=ans-nums[i];
             }
+        }*/
+        int sum1=0,sum2=0;
+        for(int i=0;i<nums.length;i+=2)
+        {
+            sum1=sum1+nums[i];
         }
+        for(int i=1;i<nums.length;i+=2)
+        {
+            sum2=sum2+nums[i];
+        }
+        ans=sum1-sum2;
         return ans;
     }
 }
